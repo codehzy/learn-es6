8a32c993a1f3adfbfa99355ad0e055fde7cd7dea
hzy <1257664828@qq.com>
2021-11-11 16:09:13 +0800

    ✨ feat(2.5):数组es6扩展

--- src/2.5.js
+++ src/2.5.js
@@ -42,3 +42,21 @@
 
 // let arr = Array.of(3);
 // console.log(arr);
+
+// let arr = Array.of(1, true, "imooc", [1, 2, 3], { name: "hzy" });
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.copyWithin(1, 3));
+
+// let arr = new Array(3).fill(7);
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.fill("hzy", 1, 3);
+// console.log(arr);
+
+// let arr = [1, 2, 3, NAN];
+// console.log(arr.indexOf(NAN)); // -1
+// console.log(NAN == NAN); // false
+// console.log(arr.includes(NaN)); // true
