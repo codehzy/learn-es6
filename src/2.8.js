08f2eda54d5a8b2d8c3e4db5db751decfa925803
hzy <1257664828@qq.com>
2021-11-15 16:11:06 +0800

    箭头函数

---
+++ src/2.8.js
@@ -0,0 +1,55 @@
+// console.log(sum(4, 5));
+
+// function sum(x, y) {
+//   return x + y;
+// }
+
+// console.log(sum);
+// let sum = function (x, y) {
+//   return x + y;
+// };
+
+// let sum = (x, y) => {
+//   return x + y;
+// };
+// let sum = (x, y) => x + y;
+// console.log(sum(3, 4));
+
+// let oBtn = document.querySelector("#btn");
+// // oBtn.addEventListener("click", function () {
+// //   setTimeout(() => {
+// //     console.log(this);
+// //   }, 1000);
+// // });
+// oBtn.addEventListener("click", function () {
+//   // setTimeout(() => {
+//   //   console.log(this);
+//   // }, 1000);
+//   setTimeout(
+//     function () {
+//       console.log(this);
+//     }.bind(this),
+//     1000
+//   );
+// });
+
+// 类
+// function People(name, age) {
+//   console.log(this);
+//   this.name = name;
+//   this.age = age;
+// }
+
+// 箭头函数模拟类的时候，Uncaught TypeError: People is not a constructor
+// let People = (name, age) => {
+//   this.name = name;
+//   this.age = age;
+// };
+
+// let hzy = new People("hzy", 23);
+// console.log(hzy);
+
+let foo = (...args) => {
+  console.log(args);
+};
+foo(1, 2, 3);
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import "./2.4";
 import "./2.5";
 import "./2.6";
 import "./2.7";
+import "./2.8";
