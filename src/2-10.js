44676d66e80d444588effd75fb65810ef3507018
hzy <1257664828@qq.com>
2021-11-15 18:04:39 +0800

    引用类型深拷贝

---
+++ src/2-10.js
@@ -0,0 +1,56 @@
+let target = {
+  a: {
+    b: {
+      c: 1,
+    },
+    e: 4,
+    f: 5,
+    g: 6,
+  },
+};
+let source = {
+  a: {
+    b: {
+      c: 1,
+    },
+    e: 2,
+    f: 3,
+  },
+};
+
+// 检查类型
+let checkType = (data) => {
+  return Object.prototype.toString.call(data).slice(8, -1);
+};
+
+let deepClone = (target) => {
+  let targetType = checkType(target);
+  let result;
+  if (targetType === "Object") {
+    result = {};
+  } else if (targetType === "Array") {
+    result = [];
+  } else {
+    return target;
+  }
+
+  for (let i in target) {
+    let value = target[i];
+    let valueType = checkType(value);
+    if (valueType === "Object" || valueType === "Array") {
+      result[i] = deepClone(value);
+    } else {
+      result[i] = value;
+    }
+  }
+  return result;
+};
+
+let obj1 = {
+  name: "hzy",
+  hobby: ["coding", "eating"],
+};
+let obj2 = deepClone(obj1);
+obj2.hobby[0] = "nice";
+console.log(obj1);
+console.log(obj2);
--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ import "./2.6";
 import "./2.7";
 import "./2.8";
 import "./2.9";
+import "./2-10";
